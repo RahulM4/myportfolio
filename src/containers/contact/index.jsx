be7b5725cc0a4b7e7fcf6ff9d211c69bf1ba1dcc
RahulM4 <rahulrps447@gmail.com>
2024-10-30 17:59:43 +0530

    update

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -1,16 +1,34 @@
 import React, { useRef, useState } from "react";
 import "./styles.scss";
 import PageHeaderContent from "../../components/pageHeaderContent";
-import {FaEnvelope} from 'react-icons/fa';
+import { FaEnvelope } from 'react-icons/fa';
 import { Animate } from "react-simple-animate";
 import emailjs from '@emailjs/browser';
 import loadingAnimation from '../../components/preLoader/lottieAnimation.json';
 import Lottie from 'lottie-react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Contact = () => {
   const form = useRef();
   const [status, setStatus] = useState(""); // State for status message
   const [isVisible, setIsVisible] = useState(false); // State to manage visibility
+
+
+  // Toast Notification
+  const handleMessageSent = () => {
+    toast.success("Message sent successfully!", {
+      position: "top-right",
+      autoClose: 3000,  // close after 3 seconds
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      theme: "dark"
+    });
+  };
+
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -18,20 +36,20 @@ const Contact = () => {
     emailjs.sendForm(
       "service_7lp79wz",
       "template_kbxny1p",
-      form.current, 
+      form.current,
       {
         publicKey: "iSYvypEudpUbpiSOI",
       }
     )
-    .then(
-      () => {
-        setStatus("Message sent successfully!"); // Set success message
-        form.current.reset(); // Clear the form if needed
-      },
-      (error) => {
-        setStatus(`Failed to send message: ${error.text}`); // Set error message
-      }
-    );
+      .then(
+        () => {
+          setStatus("Message sent successfully!"); // Set success message
+          form.current.reset(); // Clear the form if needed
+        },
+        (error) => {
+          setStatus(`Failed to send message: ${error.text}`); // Set error message
+        }
+      );
 
     // Show the status message
     setIsVisible(true);
@@ -52,7 +70,21 @@ const Contact = () => {
         headerText="Contact Me"
         icon={<FaEnvelope size={40} />}
       />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+             
       <div className="contact__content">
+
         <Animate
           play
           duration={1}
@@ -86,7 +118,7 @@ const Contact = () => {
                     required
                     name="from_email"
                     className="inputEmail"
-                    type="email"
+                    type="text"
                   />
                   <label htmlFor="email" className="emailLabel">Email</label>
                 </div>
@@ -100,13 +132,14 @@ const Contact = () => {
                   <label htmlFor="description" className="descriptionLabel">Description</label>
                 </div>
               </div>
-              <button type="submit" value="Send">Submit</button>
+              <button  type="submit" value="Send" onClick={handleMessageSent}>Submit</button>
             </form>
             {isVisible && <p className={statusClass}>{status}</p>} {/* Status message displayed here */}
           </div>
 
         </Animate>
-          <Lottie animationData={loadingAnimation} loop={true} className="lottieAnimation" />
+        <Lottie animationData={loadingAnimation} loop={true} className="lottieAnimation" />
+
       </div>
     </section>
   );
